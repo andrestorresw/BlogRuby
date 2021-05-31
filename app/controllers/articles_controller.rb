7fe09b79f756caddcc28800eec4d89f82c989bf7
Andres <im.andrestorres@gmail.com>
2021-05-31 16:46:41 -0500

    ActionText etc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,15 @@
 class ArticlesController < ApplicationController
     def new #Su funcion es crear el formulario para poder crear el articulo
-        @article = Article.new
-        @article.title = 'Demo'
+        @article = Article.new #crear un nuevo objeto tipo Articulo vacio
+        # Se crea la variable con @ para poder acceder a la variable desde la vista
+        @article.title = 'Title here'
     end
 
     def create #Guardar lo que hallamos recibido del formulario del metodo new
-        @article = Article.create(title: params[:article][:title])
+        @article = Article.create(title: params[:article][:title],content: params[:article][:content])
+        # Estamos creando un hash (JSON) para que lo guarde en el modelo(db)
+        # Recibimos lo que va a venir de new con params y creamos el articulo en la db
         render json: @article
+        # Esto muestra la version json del objeto @article que se acaba de crear en la db
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ApplicationRecord
+    has_rich_text :content #Le indicamos al modelo que tiene un campo texto enriquecido y le ponemos un identificador con nombre arbitratio (:content)
 end
