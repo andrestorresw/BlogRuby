07f7fb53c92469c34a85658895ae640e27e45239
Andres <im.andrestorres@gmail.com>
2021-05-31 21:28:03 -0500

    Edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/bienvenida', to: 'home#index' #home es el nombre del controlador y index la accion(metodo)
-  
+  get 'articles/:id', to: 'articles#show' # :id es un comodin para pasarle el articulo que queremos consultar
+
   get '/articles/new', to: 'articles#new'
   post '/articles', to:'articles#create'
+
+  get 'articles/:id/edit', to: 'articles#edit'
   
 end
